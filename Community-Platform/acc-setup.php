cb53ab9db4f1a2ae016bfd2e0b6b57fab5e96772
Sourav Deb <54738492+souravdeb299@users.noreply.github.com>
2021-07-26 19:34:08 +0530

    Add files via upload
    Account Setup Partially Fixed

--- Community-Platform/acc-setup.php
+++ Community-Platform/acc-setup.php
@@ -59,27 +59,27 @@ while ($res = mysqli_fetch_assoc($result))
             <div id="login-row" class="row justify-content-center align-items-center">
                 <div id="login-column" class="col-md-6">
                     <div id="login-box" class="col-md-12">
-                        <form id="login-form" class="form" action="setacc-process.php" method="post">
+                        <form id="login-form" class="form" action="setacc-process.php" method="get">
                             <h5 class="text-center text-white">Your Profile</h5>
 
                             <div class="form-group">
                                 <label for="username" class="text-info">Email:</label><br>
-                                <input type="email" name="email" id="email" class="form-control" placeholder="Enter Your Email" value="<?php echo $res['email']; ?>" disabled >
+                                <input type="email" name="email" id="email" class="form-control" placeholder="Enter Your Email" value="<?php echo $res['email']; ?>"  required >
                             </div>
 
                             <div class="form-group">
                                 <label for="Name" class="text-info">Name:</label><br>
-                                <input type="text" name="name" id="name" class="form-control"  placeholder="Enter Your Name" value="<?php echo $res['name']; ?>" disabled>
+                                <input type="text" name="name" id="name" class="form-control"  placeholder="Enter Your Name" value="<?php echo $res['name']; ?>"  required>
                             </div>
 
                             <div class="form-group">
                                 <label for="UserName" class="text-info">UserName:</label><br>
-                                <input type="text" name="uname" id="uname" class="form-control"  placeholder="Enter Your UserName" value="<?php echo $res['uname']; ?>" disabled>
+                                <input type="text" name="uname" id="uname" class="form-control"  placeholder="Enter Your UserName" value="<?php echo $res['uname']; ?>"  required>
                             </div>
 
                             <div class="form-group">
                                 <label for="Community" class="text-info">Community:</label><br>
-                                <input type="text" name="community" id="community" class="form-control"  placeholder="Enter Your Community" value="<?php echo $res['community']; ?>" disabled>
+                                <input type="text" name="community" id="community" class="form-control"  placeholder="Enter Your Community" value="<?php echo $res['community']; ?>"  required>
                             </div>
 
                             <?php
@@ -104,7 +104,7 @@ while ($res = mysqli_fetch_assoc($result))
                             
 
                             <div class="form-group">
-                                <button class="btn btn-dark" type="submit" value="Save" name="Save" >SAVE </button>
+                                <button class="btn btn-dark" type="submit" value="Save" name="Save" >SAVE</button>
                             </div>                            
 
                         </form>
--- Community-Platform/setacc-process.php
+++ Community-Platform/setacc-process.php
@@ -1,19 +1,19 @@
 <?php
     include 'conn.php';
 
-    if(isset($_POST['Save'])){
-        $name = $_POST['name'];
-        $email = $_POST['email'];
-        $uname = $_POST['uname'];
-        $dob = $_POST['dob'];
-        $city = $_POST['city'];
-        $state = $_POST['state'];
-        $district = $_POST['district'];
-        // $community = $_POST['community'];
+    if(isset($_GET['Save'])){
+        $name = $_GET['name'];
+        $email = $_GET['email'];
+        $uname = $_GET['uname'];
+        $dob = $_GET['dob'];
+        $city = $_GET['city'];
+        $state = $_GET['state'];
+        $district = $_GET['district'];
+        $community = $_POST['community'];
         $true = "1";
 
         // $insertquery = "INSERT into user (name,email,uname,dob,city,district,state,community,success) values('$name','$email','$uname','$dob','$city','$district','$state','$community','$true')";
-        $insertquery = "UPDATE user SET dob='$dob',city='$city', district='$district', state='$state',success='$true' WHERE name='$name'";
+        $insertquery = "UPDATE user SET dob='$dob', city='$city', district='$district', state='$state',success='$true' WHERE name='$name' ";
             if(mysqli_query($con,$insertquery))
             {
                 // echo '<script>alert("Account Setup Complete");</script>' ;
