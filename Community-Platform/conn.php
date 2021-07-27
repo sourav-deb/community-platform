6a06243840517377b5933141596b67bd4769b153
Sourav Deb <54738492+souravdeb299@users.noreply.github.com>
2021-07-27 17:07:57 +0530

    Add files via upload

--- Community-Platform/conn.php
+++ Community-Platform/conn.php
@@ -1,9 +1,7 @@
 <?php
 //      localhost
      $con=mysqli_connect('localhost','root','','community_platform');
-//      server
-    // $con=mysqli_connect('localhost','u478474012_techno3gamma','Brainkraft1@','u478474012_lab');
-        
+
     if(!$con)
     {
         die(' Please Check Your Connection'.mysqli_error($con));
